ba627c152472da604262defe2e660a6249dfa6ea
Mihai Galos <mihaigalos@gmail.com>
2022-01-28 21:22:17 +0100

    Use autoclap 0.2.0

--- src/main.rs
+++ src/main.rs
@@ -6,11 +6,7 @@ use clap::{App, Arg};
 
 #[cfg(not(tarpaulin_include))]
 fn main() {
-    let args = autoclap!()
-        .author(env!("CARGO_PKG_AUTHORS"))
-        .about(env!("CARGO_PKG_DESCRIPTION"))
-        .try_get_matches()
-        .unwrap_or_else(|e| e.exit());
+    let args = autoclap!().try_get_matches().unwrap_or_else(|e| e.exit());
 
     musage::driver::Driver::drive(args);
 }
