ac928395d26347a23c4c730f3dc31d3b230ec80d
Mihai Galos <mihaigalos@gmail.com>
2022-01-28 19:39:01 +0100

    Use autoclap 0.1.0

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,15 @@
 extern crate clap;
 extern crate colored;
-use clap::crate_version;
-use clap::{App, Arg};
+
+use autoclap::autoclap;
 
 #[cfg(not(tarpaulin_include))]
 fn main() {
-    let args = App::new(concat!(
-        env!("CARGO_CRATE_NAME"),
-        " ",
-        env!("CARGO_PKG_VERSION"),
-        " :: ",
-        concat!(
-            env!("CARGO_PKG_REPOSITORY"),
-            "/releases/tag/",
-            crate_version!()
-        )
-    ))
-    .author(env!("CARGO_PKG_AUTHORS"))
-    .about(env!("CARGO_PKG_DESCRIPTION"))
-    .arg(
-        Arg::with_name("debug")
-            .long("debug")
-            .short("d")
-            .help("Print raw data used in statistics."),
-    )
-    .get_matches_safe()
-    .unwrap_or_else(|e| e.exit());
+    let args = autoclap()
+        .author(env!("CARGO_PKG_AUTHORS"))
+        .about(env!("CARGO_PKG_DESCRIPTION"))
+        .try_get_matches()
+        .unwrap_or_else(|e| e.exit());
 
     musage::driver::Driver::drive(args);
 }
