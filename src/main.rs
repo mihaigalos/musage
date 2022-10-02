bbca3c0b536d14b27a69a427463e31e3c7fe6c05
Mihai Galos <mihaigalos@gmail.com>
2022-10-02 17:20:47 +0200

    Bump version to 0.1.2

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,24 @@ extern crate clap;
 extern crate colored;
 
 use autoclap::autoclap;
-use clap::Arg;
 use clap::Command;
+use clap::{Arg, ArgAction};
 use std::env;
 
 #[cfg(not(tarpaulin_include))]
 fn main() {
-    let app: clap::Command = autoclap!();
-    let args = app.try_get_matches().unwrap_or_else(|e| e.exit());
-    musage::driver::Driver::drive(args);
+    let app: clap::Command = autoclap!().arg(
+        Arg::new("version")
+            .long("version")
+            .short('v')
+            .action(ArgAction::SetTrue)
+            .help("Display version information."),
+    );
+    let args = app.clone().try_get_matches().unwrap_or_else(|e| e.exit());
+
+    if args.get_flag("version") {
+        println!("{}", app.get_name())
+    } else {
+        musage::driver::Driver::drive(args);
+    }
 }
